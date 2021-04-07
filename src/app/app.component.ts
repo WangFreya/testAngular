3f52e1ee6acc9ce59346000e2a3442ff3ef9a266
Freya Wang <wangfreya582@gmail.com>
2021-04-07 12:50:54 -0400

    Learned interpolation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'testAngular';
+  name = "bruce"
+  getName() {
+    return this.name
+  }
+  obj= {
+    name: 'kelly',
+    age: 20
+  }
+  arr = ['bruce','tommy','peter']
+  siteURL = window.location.href
 }
